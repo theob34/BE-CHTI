c098c19a607f3d70041920414dba09b201fe6564
Louis Chauvet <louis.chauvet@free.fr>
2021-04-06 15:46:24 +0200

    Commit initial

---
+++ Src/GestionSon.s
@@ -0,0 +1,30 @@
+	PRESERVE8
+	THUMB   
+		
+
+; ====================== zone de réservation de données,  ======================================
+;Section RAM (read only) :
+	area    mesdata,data,readonly
+
+
+;Section RAM (read write):
+	area    maram,data,readwrite
+		
+
+	
+; ===============================================================================================
+	
+
+
+		
+;Section ROM code (read only) :		
+	area    moncode,code,readonly
+; écrire le code ici		
+
+
+
+
+
+		
+		
+	END	
